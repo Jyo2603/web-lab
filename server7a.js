9b44f202a6935cf48470baf227ee3a9ce99b3130
Jyotsna <138552575+Jyo2603@users.noreply.github.com>
2025-07-03 12:02:45 +0530

    Add simple routing for engineering branches with styling

--- server7a.js
+++ server7a.js
@@ -1,39 +1,20 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send(`
-    <body style="background-color: white; color: black; font-family: sans-serif; text-align: center; padding-top: 100px;">
-      <h1>Welcome to Engineering College</h1>
-      <p><a href="/cse">CSE Department</a></p>
-      <p><a href="/mech">MECH Department</a></p>
-      <p><a href="/ece">ECE Department</a></p>
-    </body>
-  `);
+app.get('/', (req, res) => {
+  res.send('<body style="font-family:sans-serif;text-align:center;background:#f0f0f0;"><h2>Welcome</h2><a href="/cse">CSE</a> | <a href="/ece">ECE</a> | <a href="/mech">MECH</a></body>');
 });
 
-app.get("/cse", (req, res) => {
-  res.send(`
-    <body style="background-color: lightblue; color: black; font-family: Arial; text-align: center; padding-top: 100px;">
-      <h1>CSE Department</h1>
-    </body>
-  `);
+app.get('/cse', (req, res) => {
+  res.send('<body style="background:lightblue;font-family:Arial;text-align:center;"><h2>CSE Dept</h2></body>');
 });
 
-app.get("/mech", (req, res) => {
-  res.send(`
-    <body style="background-color: lightcoral; color: white; font-family: Courier New; text-align: center; padding-top: 100px;">
-      <h1>MECH Department</h1>
-    </body>
-  `);
+app.get('/ece', (req, res) => {
+  res.send('<body style="background:lightgreen;font-family:Georgia;text-align:center;"><h2>ECE Dept</h2></body>');
 });
 
-app.get("/ece", (req, res) => {
-  res.send(`
-    <body style="background-color: lightgreen; color: black; font-family: Georgia; text-align: center; padding-top: 100px;">
-      <h1>ECE Department</h1>
-    </body>
-  `);
+app.get('/mech', (req, res) => {
+  res.send('<body style="background:lightcoral;font-family:Courier;text-align:center;"><h2>MECH Dept</h2></body>');
 });
 
-app.listen(3000, () => console.log("Server running at http://localhost:3000"));
+app.listen(3000, () => console.log('http://localhost:3000'));
