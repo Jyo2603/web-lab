ab821580199c486b12ded1a32f947fa8672669b8
Jyotsna <138552575+Jyo2603@users.noreply.github.com>
2025-07-03 17:54:07 +0530

    Update index8b.html and server8b.js

--- server8b.js
+++ server8b.js
@@ -1,45 +1,34 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const path = require('path');
-
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname));
+app.use(express.static(__dirname)); // to serve HTML
 
-// Serve the form
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index8b.html'));
 });
 
-MongoClient.connect('mongodb://localhost:27017').then(client => {
-  const db = client.db('shopDB').collection('products');
+MongoClient.connect('mongodb://127.0.0.1:27017')
+  .then(client => {
+    const db = client.db('examDB').collection('products');
 
-  // Add product
-  app.post('/add', async (req, res) => {
-    const price = parseFloat(req.body.Price);
-    const discount = parseFloat(req.body.Discount);
-    const stock = parseInt(req.body.Stock);
-    const final = price - (price * discount / 100);
+    // Add product with Final Price calculation
+    app.post('/product', (req, res) => {
+      const data = req.body;
+      data.Price = parseFloat(data.Price);
+      data.Discount = parseFloat(data.Discount);
+      data.Final_Price = data.Price - (data.Price * data.Discount / 100);
+      db.insertOne(data).then(() => res.send('Product Added'));
+    });
 
-    const product = {
-      Product_ID: req.body.Product_ID,
-      Name: req.body.Name,
-      Price: price,
-      Discount: discount,
-      Stock: stock,
-      Final_Price: final
-    };
+    // GET products with Final_Price < 1000
+    app.get('/product/cheap', (req, res) => {
+      db.find({ Final_Price: { $lt: 1000 } }).toArray()
+        .then(data => res.json(data));
+    });
 
-    await db.insertOne(product);
-    res.send('Product added');
+    app.listen(3000, () => console.log('http://localhost:3000'));
   });
-
-  // Get products with Final_Price < 1000
-  app.get('/cheap', async (req, res) => {
-    const result = await db.find({ Final_Price: { $lt: 1000 } }).toArray();
-    res.json(result);
-  });
-
-  app.listen(3000, () => console.log('http://localhost:3000'));
-});
